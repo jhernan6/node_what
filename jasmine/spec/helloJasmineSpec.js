ea1fa72ac77093f5839463da8afab846a6ca6c75
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-22 23:04:26 -0700

    Edits to test file

--- jasmine/spec/helloJasmineSpec.js
+++ jasmine/spec/helloJasmineSpec.js
@@ -14,6 +14,7 @@ describe("Hello Jasmine Server", function(){
 		it("returns Hello Jasmine", function(done){
 			request.get(base_url, function(error, response, body){
 				expect(body).toBe("Hello Jasmine");
+				done();
 			});
 		});
 	});
