adbaad626c3eee347d0717a94482e52ec4ea183a
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-30 22:34:04 -0700

    Updated flight class

--- beaker2/flight/index.js
+++ beaker2/flight/index.js
@@ -1,21 +1,39 @@
-var number, origin, destination;
-
-exports.setNumber = function(num){
-	number = num;
-};
-
-exports.setOrigin = function(orig){
-	origin = orig;
-};
-
-exports.setDestination = function(dest){
-	destination = dest;
-};
-
-exports.getInfo = function(){
-	return {
-		number: number,
-		origin: origin,
-		destination: destination
+var Flight = function(){
+	this.data = {
+		number: null,
+		origin: null,
+		destination: null,
+		departs: null,
+		arrives: null,
+		actualDepart: null,
+		actualArrive: null
 	};
+	
+	this.fill = function(info) {
+		for(var prop in this.data) {
+			if(this.data[prop] !== 'undefined') {
+				this.data[prop] = info[prop];
+			}
+		};
+	};
+
+	this.triggerDepart = function(){
+		this.data.actualDepart = Date.now();
+	};
+
+	this.triggerArrive = fucntion() {
+		this.data.actualArrive = Date.now();
+	};
+
+	this.getInformation = function() {
+		return this.data;
+	};
+};
+
+module.exports = function(info) {
+	var instance = new Flight();
+
+	instance.fill(info);
+
+	return instance;
 };
