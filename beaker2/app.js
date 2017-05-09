60b4c9186c8c01fcfbaad60d8525377c6d53a88c
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-05-08 23:23:51 -0700

    Removed all server code from file and exported it

--- beaker2/app.js
+++ beaker2/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var routes = require('./routes');
-var http = require('http');
 var path = require('path');
 
 var app = express();
@@ -30,6 +29,4 @@ app.get('/flight/:number', routes.flight);
 app.put('/flight/:number/arrived', routes.arrived);
 app.get('/list', routes.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+module.exports = app;
