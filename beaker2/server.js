6d5dd210a1020abf0aa86ee282f11b569dcbd749
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-05-09 00:10:13 -0700

    Updated server file to work with modules

--- beaker2/server.js
+++ beaker2/server.js
@@ -1,5 +1,6 @@
 var http = require('http');
-var app = require('./app');
+var flights = require('./data');
+var app = require('./app')(flights);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
