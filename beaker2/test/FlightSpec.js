ffc3e1848832a70155afd511a0c0f7eac4e8fb98
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-05-10 00:51:25 -0700

    Created real test for API

--- beaker2/test/FlightSpec.js
+++ beaker2/test/FlightSpec.js
@@ -5,12 +5,38 @@ var supertest = require('supertest');
 
 describe('Flights test',function(){
 	
-	it('should pass', function(done){
-		done();
+	it('should return valid data for flght 18', function(done){
+		supertest(app)
+		.get('/flight/18')
+		.end(function (err,res){
+			res.status.should.equal(200);
+			done();
+		});
 	});
 	
-	it('should not pass', function(done){
-		throw "Don't pass";
-		done();
+	it('should return error for invalid flight', function(done){
+		supertest(app)
+		.get('/flight/38')
+		.end(function (err, res){
+			res.status.should.equal(404);
+			done();
+		});
+	});
+
+	it('should mark a flight as arrived',function(done){
+		supertest(app)
+		.put('/flight/18/arrived')
+		.end(function (err, res){
+			res.status.should.equal(200);
+			res.body.status.should.equal('Done');
+
+			supertest(app)
+			.get('/flight/18')
+			.end(function (err, res){
+				res.status.should.equal(200);
+				res.body.actualArrive.should.not.equal(undefined);
+				done();
+			});
+		});
 	});
 });
