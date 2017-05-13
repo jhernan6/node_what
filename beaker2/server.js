d0aee471686cfbc84f3092fe5b2bfb41219860da
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-05-12 23:54:47 -0700

    Made server file aware of the database file

--- beaker2/server.js
+++ beaker2/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var flights = require('./data');
 var app = require('./app')(flights);
+var db = require('./db');
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
